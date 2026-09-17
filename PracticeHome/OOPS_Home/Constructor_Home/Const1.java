5f96c3088afd0b90cb6ec1007229262886b73d20
Abhinav Maurya <navrya1212@gmail.com>
2026-09-17 22:54:13 +0530

    new .java file added

---
+++ PracticeHome/OOPS_Home/Constructor_Home/Const1.java
@@ -0,0 +1,25 @@
+package PracticeHome.OOPS_Home.Constructor_Home;
+
+public class Const1 {
+    public static void main(String args[]){
+        Student s1= new Student();
+        s1.name="abhi";
+        s1.age=23;
+        s1.college="hit";
+       Student s2=new Student();
+       
+
+    }
+
+ 
+}
+
+   class Student{
+        String name;
+        int age;
+        String college;
+
+
+
+
+    }
